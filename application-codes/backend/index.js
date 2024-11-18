14d9ad00e846878a00054e1532c8e9d2f34ea358
chuongdinh <dinhvanchuong2k@gmail.com>
2024-11-18 14:51:35 +0700

    add logs for api server

--- application-codes/backend/index.js
+++ application-codes/backend/index.js
@@ -3,9 +3,15 @@ const connection = require("./db");
 const cors = require("cors");
 const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+
+console.log("getting env: ", process.env.MONGO_USERNAME ?? "no username");
+
+console.log("intialize app");
 
 connection();
+
+console.log("runhere");
 
 app.use(express.json());
 app.use(cors());
@@ -13,31 +19,31 @@ app.use(cors());
 // Health check endpoints
 
 // Basic health check to see if the server is running
-app.get('/healthz', (req, res) => {
-    res.status(200).send('Healthy');
+app.get("/healthz", (req, res) => {
+  res.status(200).send("Healthy");
 });
 
-let lastReadyState = null;  
+let lastReadyState = null;
 // Readiness check to see if the server is ready to serve requests
-app.get('/ready', (req, res) => {
-    // Here you can add logic to check database connection or other dependencies
-    const isDbConnected = mongoose.connection.readyState === 1;
-    if (isDbConnected !== lastReadyState) {
-        console.log(`Database readyState: ${mongoose.connection.readyState}`);
-        lastReadyState = isDbConnected;
-    }
-    
-    if (isDbConnected) {
-        res.status(200).send('Ready');
-    } else {
-        res.status(503).send('Not Ready');
-    }
+app.get("/ready", (req, res) => {
+  // Here you can add logic to check database connection or other dependencies
+  const isDbConnected = mongoose.connection.readyState === 1;
+  if (isDbConnected !== lastReadyState) {
+    console.log(`Database readyState: ${mongoose.connection.readyState}`);
+    lastReadyState = isDbConnected;
+  }
+
+  if (isDbConnected) {
+    res.status(200).send("Ready");
+  } else {
+    res.status(503).send("Not Ready");
+  }
 });
 
 // Startup check to ensure the server has started correctly
-app.get('/started', (req, res) => {
-    // Assuming the server has started correctly if this endpoint is reachable
-    res.status(200).send('Started');
+app.get("/started", (req, res) => {
+  // Assuming the server has started correctly if this endpoint is reachable
+  res.status(200).send("Started");
 });
 
 app.use("/api/tasks", tasks);
